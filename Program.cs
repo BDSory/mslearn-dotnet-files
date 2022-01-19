4c122f05980a9712405d90d839bef5a70b67a6db
Bryan Sory <superbee45@gmail.com>
2022-01-19 15:53:57 -0600

    update salesTotal

--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
 ﻿using System.IO;
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
+
 var salesFiles = FindFiles(storesDirectory);
 
-foreach (var file in salesFiles)
+var salesTotal = CalculateSalesTotal(salesFiles);
+
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
 {
-    Console.WriteLine(file);
+    double salesTotal = 0;
+    foreach (var file in salesFiles)
+    {
+        string salesJson = File.ReadAllText(file);
+        SalesData? data = JsonSerializer.Deserialize<SalesData?>(salesJson);
+        salesTotal += data?.Total ?? 0;
+    }
+    return salesTotal;
 }
 
 IEnumerable<string> FindFiles(string folderName)
@@ -18,7 +35,6 @@ IEnumerable<string> FindFiles(string folderName)
 
     foreach (var file in foundFiles)
     {
-        //The file name will contain the full path, so only check the end of it
         var extension = Path.GetExtension(file);
         if (extension == ".json")
         {
@@ -28,5 +44,5 @@ IEnumerable<string> FindFiles(string folderName)
 
     return salesFiles;
 }
-//string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-//Console.WriteLine(docPath);
+
+record SalesData (double Total);
